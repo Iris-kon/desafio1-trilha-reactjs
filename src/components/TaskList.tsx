78005db6690a16d059e6c6ebd00d624ba950ff16
Thiago Oliveira <thiagooliveira.k.on@gmail.com>
2021-09-24 22:23:48 -0300

    add the funtionalities for complete the desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -10,20 +10,55 @@ interface Task {
   isComplete: boolean;
 }
 
+function generateRandomId(tasksIds: number[]) {
+  let id = Math.floor(Math.random() * (99 - 1)) + 1
+
+  //test if id is already used and call the function again if needed
+  if(tasksIds.includes(id)) generateRandomId(tasksIds)
+
+  return id
+}
+
 export function TaskList() {
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [tasks, setTasks] = useState<Task[]>([])
+  const [newTaskTitle, setNewTaskTitle] = useState('')
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle) {
+      //generate a unique random id
+      const id = generateRandomId(tasks.map(t => t.id))
+      
+      //append new task at the end of the array
+      setTasks([...tasks, {id, title: newTaskTitle, isComplete: false}])
+    }else{
+      alert('O nome da task não pode ser vazio')
+    }
+
+    //clear newTastkTitle
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const oldTasks = tasks
+
+    //find the position of the task with the given id in the array
+    const idIndex = oldTasks.findIndex(t => t.id === id)
+
+    //set the taks as complete or incomplete
+    oldTasks[idIndex].isComplete = !oldTasks[idIndex].isComplete;
+
+    //set the new value in tasks state
+    setTasks([...oldTasks])
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    //filter the tasks array to remove the task with the given id
+    const newTasks = tasks.filter(t => t.id !== id)
+    
+    setTasks(newTasks)
   }
 
   return (
